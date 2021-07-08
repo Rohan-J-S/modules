3394f3258eb390d0657f9a87f26641a4a21481fe
Rohan <53927306+Rohan-J-S@users.noreply.github.com>
2021-07-08 14:42:38 +0530

    Update sort_module.py

--- sort_module.py
+++ sort_module.py
@@ -1,11 +1,10 @@
 def bubble_sort(l):
+    """
+#input params: unsorted list
+#output params: sorted list"""
     for y in range(len(l)):
         for x in range(len(l)):
             if x != len(l) - 1:
                 if l[x+1] < l[x]:
                     l[x+1],l[x] = l[x],l[x+1]  
                    
-    return (l)
-
-#input params: unsorted list
-#output params: sorted list
